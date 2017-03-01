bbd500b5227ab79baa8819b0540bdde92e25de0f
Nick Cloward <nick@authentise.com>
2017-03-01 10:02:46 -0700

    Add controller TODO's
    Adds controller todo's which add asynchronous functionality to simulate
    elevators that are really async.
    
    Adds todo for requesting an elevator to a target floor based on all the
    elevators priorities to that floor.

--- ele/controller.py
+++ ele/controller.py
@@ -6,4 +6,14 @@ def process(elevator_count, max_floor):
     for index in range(elevator_count):
         elevators = Elevator(max_floor)
 
+    # TODO: When the the controller would get a request for the closest elevator to go to a
+    # specific target floor it would calculate the priority for each elevator and call
+    # the move on the elevator with the lowest priority and move it to the correct floor.
+
+    # TODO: Implement a way to asynchronously communicate with each elevator.
+    # This could be using threading or asyncio.  It would allow the elevators to
+    # use more of an event communication with the controller.  This would make it easier
+    # to interupt the moving elevator as it moves passed a floor that has requested
+    # and elevator.
+
     return True
--- tests/test_controller.py
+++ tests/test_controller.py
@@ -2,3 +2,14 @@ import ele.controller
 
 def test_process_success():
     assert ele.controller.process(4, 10) is True
+
+def test_request_elevator():
+    # Tests requesting an elevator, evaluating the priority and
+    # picking the elevator with the lowest priority.
+    pass
+
+def test_request_elevator_moving():
+    # Tests requesting an elevator, evaluating all the elevators priorities
+    # and then making sure the elevator that is moving passed the target
+    # floor responds to the request.
+    pass
